6fc20d3430d5e9c77c241c907bef63c0d8e92757
saraesterlevy <sarale354@gmail.com>
2023-01-04 18:50:09 -0500

    Adding all current project files to repository

---
+++ rsvp/script.js
@@ -0,0 +1,24 @@
+// const button = document.querySelector(".button")
+//
+// button.addEventListener("click", e => {
+//     console.log(".button 1");
+// })
+//
+//
+// button.addEventListener("click", e => {
+//     console.log(".button 2");
+// })
+
+//
+// if (4+3 === 7) {
+//     alert("Click ok");
+// }
+// function sayHello() {
+//     console.log("Hello world ");
+// }
+//
+//
+// console.log(sayHello());
+//
+// console.log("hello")
+
