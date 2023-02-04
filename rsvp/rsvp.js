7b525b999e6546aa00bbc2130d4dd189e5d49b1b
saraesterlevy <sarale354@gmail.com>
2023-02-03 19:46:39 -0500

    saving user input as table and trying to export into file, such as excel, csv, and json

--- rsvp/rsvp.js
+++ rsvp/rsvp.js
@@ -14,5 +14,8 @@ window.writeValues = function(form) {
         }
 
         document.getElementById('fullname').innerHTML = 'Name: ' + name + '<br>' + 'Guest: ' + guest + '<br>' + 'Allergies: ' + allergies + '<br>' + 'RSVP' + '&nbsp&nbsp&nbsp' + 'Confirmed';
+        // take name, guest, and allergies and insert them into an excel file
+        document.getElementById('poop').innerHTML = '<table><tr><th>Name</th><th>Allergy</th><th>Guest</th></tr><tr><th>' + name + '</th><th>' + guest + '</th><th>' + allergies + '</th>'
+        window.open('data:text/csv;charset=utf-8,' + $('#poop').html());
     }
 }
